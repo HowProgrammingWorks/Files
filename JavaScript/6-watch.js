f9731fcb373c5137a3f95ca8f2b7ca2fe998b8cb
tshemsedinov <timur.shemsedinov@gmail.com>
2017-09-14 13:31:28 +0300

    Add autoreload example

--- JavaScript/6-watch.js
+++ JavaScript/6-watch.js
@@ -3,7 +3,7 @@
 const fs = require('fs');
 
 const watch = (path) => {
-  const watcher = fs.watch(path, (event, file) => {
+  fs.watch(path, (event, file) => {
     console.dir({ event, file });
   });
 };
---
+++ JavaScript/7-autoreload.js
@@ -0,0 +1,26 @@
+'use strict';
+
+const fs = require('fs');
+
+let buffer;
+
+const load = (path) => {
+  fs.readFile(path, (err, data) => {
+    if (err) throw err;
+    buffer = data;
+    console.log('\x1Bc');
+    console.log('Buffer length: ' + buffer.length);
+    console.log(buffer.toString());
+  });
+};
+
+const watch = (path) => {
+  fs.watch(path, (event, file) => {
+    console.dir({ event, file });
+    load(path);
+  });
+};
+
+const path = './1-readFileSync.js';
+load(path);
+watch(path);
