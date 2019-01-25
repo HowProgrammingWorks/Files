8a61c71e89e52edeaf827456a4ad9478f3de91d8
tshemsedinov <timur.shemsedinov@gmail.com>
2019-01-25 20:23:31 +0200

    Get string instead of Buffer

--- JavaScript/1-readFileSync.js
+++ JavaScript/1-readFileSync.js
@@ -2,10 +2,6 @@
 
 const fs = require('fs');
 
-const buffer = fs.readFileSync('1-readFileSync.js');
-console.log(`Buffer length: ${buffer.length}`);
-console.log(buffer);
-
 const data = fs.readFileSync('1-readFileSync.js', 'utf8');
 console.log(`Data length: ${data.length}`);
 console.log(data);
