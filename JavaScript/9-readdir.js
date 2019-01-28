3dbddbf49e5f7d2024eb176f443d64f5f434ee89
tshemsedinov <timur.shemsedinov@gmail.com>
2019-01-28 23:13:38 +0200

    Add readdir examples

---
+++ JavaScript/9-readdir.js
@@ -0,0 +1,13 @@
+'use strict';
+
+const fs = require('fs');
+
+
+const output = (err, files) => {
+  if (err) console.log(err);
+  else console.dir(files);
+};
+
+fs.readdir('.', output);
+fs.readdir('..', output);
+fs.readdir('./lib', output);
