3768f6b1ab1a67920c77a634289832b409eb8340
Timur Shemsedinov <timur.shemsedinov@gmail.com>
2023-07-13 18:45:30 +0300

    Apply suggestions from code review

--- JavaScript/4-error-fstat.js
+++ JavaScript/4-error-fstat.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const fs = require('node:fs');
+const fs = require('node:fs').promises;
 
 const fileNames = ['1-readFileSync.js', 'n-untitled.js', '3-async.js'];
 const promises = fileNames.map((fileName) => fs.lstat(fileName));
