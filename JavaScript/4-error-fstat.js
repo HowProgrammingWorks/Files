1fd0f32243d6612e8a54418ff51179f448e9c248
Konstantin <zemliakov.konstantin@gmail.com>
2023-07-13 21:46:09 +0600

    Update 4-error-fstat.js
    PR-URL: https://github.com/HowProgrammingWorks/Files/pull/5

--- JavaScript/4-error-fstat.js
+++ JavaScript/4-error-fstat.js
@@ -1,26 +1,7 @@
 'use strict';
 
-const fs = require('node:fs');
+const fs = require('node:fs').promises;
 
-const files = ['1-readFileSync.js', 'n-untitled.js', '3-async.js'];
-
-const stats = new Array(files.length);
-
-let rest = files.length;
-
-const printResult = () => {
-  console.dir({ stats });
-};
-
-files.forEach((file, i) => {
-  console.dir({ file, i });
-  fs.lstat(file, (err, stat) => {
-    if (err) {
-      console.log(`File ${file} not found`);
-    } else {
-      stats[i] = stat;
-    }
-    if (--rest) return;
-    printResult();
-  });
-});
+const fileNames = ['1-readFileSync.js', 'n-untitled.js', '3-async.js'];
+const promises = fileNames.map((fileName) => fs.lstat(fileName));
+Promise.allSettled(promises).then(console.dir);
